7a965555fd471cb748f2b6aa7ae4fb16186d6c6f
Gonzalo Lázaro <glazzam1005@g.educaand.es>
2023-11-11 15:47:30 +0100

    Primer commit

---
+++ js/smite.js
@@ -0,0 +1,8 @@
+/*******************************************************************************
+*                                                                              *
+*                  Página de javascript para la web de SMITE                   *
+*                  Autor: Gonzalo Lázaro Zambrano                              * 
+*                  Trabajo primer trimestre                                    *
+*                  Curso: 1º DAW                                               *
+*                                                                              *
+*******************************************************************************/
